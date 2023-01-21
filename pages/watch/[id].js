d6b37da4e15207592c037eb0925095d443ac7a62
xerox0213 <nasreddine021@gmail.com>
2023-01-21 16:51:39 +0100

    Rajout de commentaires explicatif

--- pages/watch/[id].js
+++ pages/watch/[id].js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from '/styles/VideoPlayer.module.css';
 
 function VideoPlayer(props) {
+  // Puisqu'on ne reçoit que la key de la vidéo on se sert alors d'un iframe proposé par YouTube pour afficher notre vidéo
   return (
     <>
       <div className={styles.videoPlayer}>
@@ -23,7 +24,9 @@ function VideoPlayer(props) {
 export default VideoPlayer;
 
 export async function getServerSideProps(context) {
+  // Récupère l'ID de l'url (slug) pour utilisation lors du fetch des infos du film ou de la série correspondant à cet ID
   const id = context.params.id;
+  // Récupère le mediaType envoyé en paramètre de le l'URL pour faire la bonne requête de type movie ou tv
   const mediaType = context.query.mediaType;
 
   try {
@@ -31,13 +34,13 @@ export async function getServerSideProps(context) {
       `https://api.themoviedb.org/3/${mediaType}/${id}/videos?api_key=c6a3bad00e21476c3f2e75f7e8893c2d&language=en-US`
     );
     const data = await request.json();
+    // On ne garde que les éléments qui respectent l'ensemble des conditions
     const dataFiltered = data.results.find(
       (elem) =>
         elem.type === 'Trailer' &&
         elem.official === true &&
         elem.site === 'YouTube'
     );
-
     if (data.status_code === 34 || !dataFiltered) {
       throw 'erreur 404';
     }
