6fc624522badc7b2721efd2040f9c1fb0b4a5a3e
xerox0213 <nasreddine021@gmail.com>
2023-01-22 11:46:47 +0100

    Test requête POST cookies

--- pages/api/user_api/logout.js
+++ pages/api/user_api/logout.js
@@ -4,20 +4,22 @@ import { signOut } from 'firebase/auth';
 
 export default async function handler(req, res) {
   try {
-    // Déconnexion de l'utilisateur
-    await signOut(auth);
-    // Supression du cookie token en l'expirant
-    res.setHeader(
-      'Set-Cookie',
-      cookie.serialize('token', '', {
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
-        maxAge: 0,
-        sameSite: 'strict',
-        path: '/',
-      })
-    );
-    res.status(200).json({ code: 200 });
+    if (req.method === 'POST') {
+      // Déconnexion de l'utilisateur
+      await signOut(auth);
+      // Supression du cookie token en l'expirant
+      res.setHeader(
+        'Set-Cookie',
+        cookie.serialize('token', '', {
+          httpOnly: true,
+          secure: process.env.NODE_ENV !== 'development',
+          maxAge: 0,
+          sameSite: 'strict',
+          path: '/',
+        })
+      );
+      res.status(200).json({ code: 200 });
+    }
   } catch (error) {
     res.status(401).json(error);
   }
